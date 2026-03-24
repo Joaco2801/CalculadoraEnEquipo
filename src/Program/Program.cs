09ed49e4be884753148bd557ae45f32310d1dc62
candeecarbone <candelacarbone@gmail.com>
2026-03-24 20:44:31 -0300

    resta

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,9 +2,9 @@
 {
     public static void Main()
     {
-        Console.WriteLine(Suma.Sumar(1, 2));
+        //  Console.WriteLine(Suma.Sumar(1, 2));
         Console.WriteLine(Resta.Restar(3, 4));
-        Console.WriteLine(Multiplicacion.Multiplicar(5, 6));
-        Console.WriteLine(Division.Dividir(7, 8));
+        // Console.WriteLine(Multiplicacion.Multiplicar(5, 6));
+        // Console.WriteLine(Division.Dividir(7, 8));
     }
 }
---
+++ src/Program/Resta.cs
@@ -0,0 +1,8 @@
+// Esta clase implementa la operación resta
+public class Resta
+{
+    public static int Restar(int a, int b)
+    {
+        return a - b;
+    }
+}
